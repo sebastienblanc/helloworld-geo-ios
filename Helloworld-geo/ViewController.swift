f0bbc463c883b86b0352df7407a1e1227d2e4b8e
sebastienblanc <scm.blanc@gmail.com>
2015-01-18 17:33:26 +0100

    Initial Commit

---
+++ Helloworld-geo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Helloworld-geo
+//
+//  Created by sebi-mbp on 18/01/15.
+//  Copyright (c) 2015 sebi-mbp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
